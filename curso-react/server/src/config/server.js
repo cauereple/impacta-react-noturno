8b9967d6941a7eece4589cfbd3a4ce91c5b0a41e
cauereple <cauereple@users.noreply.github.com>
2019-12-11 20:01:48 -0300

    adicionando comentários

--- curso-react/server/src/config/server.js
+++ curso-react/server/src/config/server.js
@@ -1,16 +1,18 @@
-const port = 3200
+const port = 3200 // constante para guardar a porta
 
 //middlewares - singletons: commons js
-const bodyParser = require('body-parser')
-const express = require('express')
+const bodyParser = require('body-parser') // bodyParser ajuda a limitar a entrada e a saída do nosso programa
+const express = require('express') // Servidor express
 
 const server = express() //novo servidor, baseado no express
+//.use é uma função do express
 
 //para toda requisição que chegar, use o bodyparser para
 //interpretar chegadas no formato urlencoded
 server.use(bodyParser.urlencoded({extended : true}))
 
 //considera o formato json no corpo da requisição
+//Nosso servidor só vai receber json
 server.use(bodyParser.json())
 
 server.listen(port, function(){
