3a6c0e0bebf269b6ac0ce53466dc3be9aa9ae380
cauereple <cauereple@users.noreply.github.com>
2019-12-16 19:35:06 -0300

    fazendo desctructor no cabeçalho

--- curso-react/site/src/componentes/cabecalho.js
+++ curso-react/site/src/componentes/cabecalho.js
@@ -1,15 +1,13 @@
 import React from 'react'
 
 export class Cabecalho extends React.Component {
-    render() {
-
-        
+    render() {     
         const {titulo, subtitulo} = this.props // Destructor
 
         return (
             <header className="pb-2 mt-4 mb-2 border-bottom">
-                <h2><strong>{this.props.titulo}</strong> -
-                <small>{this.props.subtitulo}</small></h2>
+                <h2><strong>{titulo}</strong> -
+                <small>{subtitulo}</small></h2>
             </header>
         )
     }
