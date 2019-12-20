4317a14de6f6a72c9115f04f9c5302c84db928e2
cauereple <cauereple@users.noreply.github.com>
2019-12-19 21:20:06 -0300

    terminando o reducer do formulário

--- curso-react/site/src/reducers/contatoReducer.js
+++ curso-react/site/src/reducers/contatoReducer.js
@@ -1,15 +1,19 @@
 const INITIAL_STATE = {
-    data: '2019-12-11T22:12:32.560Z',
-    nome: 'Cauê',
-    email: 'caue-reple@hotmail.com',
-    telefone: 11983224815,
-    assunto: 'Informações sobre React'
+    data: '',
+    nome: '',
+    email: '',
+    telefone: '',
+    assunto: ''
 }
 //state = INITIAL_STATE recebo um parametro, se esse parametro estiver indefinido, ele será o valor colocado depois do igual. o Nome disso é DEFAULT PARAMETER
 export default function(state = INITIAL_STATE, action){ // Definição padrão de um redutor --> export default com state e action
     switch(action.type){ // switch é um controlador de fluxo. Ele olha cada if, em vez de fazermos vários if's
         case 'ATUALIZA_DATA' : return {...state, data : action.value}
         case 'ATUALIZA_NOME' : return {...state, nome : action.value}
+        case 'ATUALIZA_EMAIL' : return {...state, email : action.value}
+        case 'ATUALIZA_TELEFONE' : return {...state, telefone : action.value}
+        case 'ATUALIZA_ASSUNTO' : return {...state, assunto : action.value}
+        case 'LIMPAR_FORM' : return INITIAL_STATE
         default : return state
     }
 }
