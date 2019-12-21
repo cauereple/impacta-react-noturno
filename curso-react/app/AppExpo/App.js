c95aedc134e4880adc4e57f8226c08f6e9f1d59a
cauereple <cauereple@users.noreply.github.com>
2019-12-20 22:01:23 -0300

    commit final Curso React - criação e finalização app expo

---
+++ curso-react/app/AppExpo/App.js
@@ -0,0 +1,28 @@
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import Home from './screens/home'
+import Contatos from './screens/contato'
+import Cursos from './screens/cursos'
+
+const Stack = createStackNavigator({ 
+  Home: {
+    navigationOptions : {
+      header : null
+    },
+    screen: Home
+  },
+  Cursos: {
+    navigationOptions : {
+      title: 'Cursos'
+    },
+    screen: Cursos
+  },
+  Contatos: {
+    navigationOptions : {
+      title: 'Contatos'
+    },
+    screen: Contatos
+  }
+})
+
+export default createAppContainer(Stack)
